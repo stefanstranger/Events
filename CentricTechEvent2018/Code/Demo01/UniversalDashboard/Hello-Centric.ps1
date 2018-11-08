c5e741fa99c7ea3e72b491e1710f44eac311f376
Stefan Stranger <stefan@stranger.nl>
2018-11-08 13:39:24 +0100

    Update title for demo purposes

--- CentricTechEvent2018/Code/Demo01/UniversalDashboard/Hello-Centric.ps1
+++ CentricTechEvent2018/Code/Demo01/UniversalDashboard/Hello-Centric.ps1
@@ -1,4 +1,4 @@
-$CentricDashboard = New-UDDashboard -Title "Hello Centric - Demo" -Content {
+$CentricDashboard = New-UDDashboard -Title "Hello Centric - Twitter Sentiment Demo " -Content {
     New-UDCard -Title 'Centric Tech Event 2018 - Infrastructure-track' -Image (New-UDImage -Url 'https://www.centric.eu/media/d02c184e-6f5d-40ed-a61f-593947b5f64f/6Bh_qg/Events/Netherlands/EventPhotos/Craft/Tech%20event%202018/bnr-TEX-mail.jpg') -Content {
         'Infrastructure as Code door Stefan Stranger (Microsoft)'
     } -Links @(
